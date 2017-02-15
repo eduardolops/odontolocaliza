89d471f56fbd0988f0ee14d37332e4c6f3682df2
Eduardo Lopes <edulopestj@gmail.com>
2017-02-15 00:31:50 -0200

    corrigindo erro de ortográfia na pagina de contato

--- resources/views/layout/pages/contact.blade.php
+++ resources/views/layout/pages/contact.blade.php
@@ -50,7 +50,7 @@
 <div class="contact_us_form">
     <div class="container">
         {!! Form::open([ 'route' => 'contact_store', 'name' => 'contact', 'method' => 'post' ]) !!}
-        <h4>Ficamos muito contentes com o seu contato, diga um olá para a Odontolocalza!</h4>
+        <h4>Ficamos muito contentes com o seu contato, diga um olá para a Odontolocaliza!</h4>
         <div class="input_wrapper row">
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 block_two">
                 @if (session('message'))
